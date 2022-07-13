aabaf8a788db4755a933cee8baf48bde5830bb3e
Andrei Marakulin <99500512+AndreiMarakulin@users.noreply.github.com>
2022-07-13 22:53:28 +0500

    Fix connection to DB

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,35 +1,22 @@
-require('dotenv').config()
+require("dotenv").config();
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  development: {
-    client: 'postgresql',
-    connection: {
-      host : process.env.DB_HOST,
-      port : process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      user:     process.env.DB_USER,
-      password: process.env.DB_PASSWORD
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './db/migrations'
-    },
-    seeds: {
-      directory: './db/seeds'
-    }
+  client: "postgresql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
   },
-  production: {
-    client: 'postgresql',
-    connection: process.env.DB_URL,
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './db/migrations'
-    },
-    seeds: {
-      directory: './db/seeds'
-    }
-  }
+  migrations: {
+    tableName: "knex_migrations",
+    directory: "./db/migrations",
+  },
+  seeds: {
+    directory: "./db/seeds",
+  },
 };
